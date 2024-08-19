cc588667dbabe5f5e5787a7ad8234ee6bda59012
xiangru-chen <160882968+xiangru-chen@users.noreply.github.com>
2024-08-19 08:58:24 +0800

    Update main.py
    Add time counting

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import summary
 import speech_to_text
 
 import streamlit as st
-
+import time
 
 # Streamlit UI
 col1, col2 = st.columns(2)
@@ -17,12 +17,21 @@ speech_to_text.start_whisper()
 
 
 while True:
+    # print out the spending time
+    start_time_output = time.time()
     output = speech_to_text.get_output()
+    end_time_output = time.time()
+    print(f"speech_to_text spent {end_time_output - start_time_output} seconds")
+    
     if output is None:
         break
     print(output, flush=True)
     col1.markdown(output)
+    # print out the spending time
+    start_time_output = time.time()
     response = summary.get_response(output)
+    end_time_output = time.time()
+    print(f"summary spent {end_time_output - start_time_output} seconds")
     col2.markdown(response)
 
 speech_to_text.get_error()
